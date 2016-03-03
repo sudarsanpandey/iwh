87687bdbbe30f6d8d3c600fdae967f669c4791af
Sudarsan Pandey <spandey@AD.YDESIGNGROUP.COM>
2016-03-02 19:32:32 -0800

    Unsuccessful Login attempt Test - Successful

--- app/src/main/java/com/example/spandey/iwh/MainActivity.java
+++ app/src/main/java/com/example/spandey/iwh/MainActivity.java
@@ -21,10 +21,12 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONObject;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -40,7 +42,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-	private static LOGIN_URL="http://wcsc02.ad.ydesigngroup.com/login";
+	private static String LOGIN_URL="http://wcsc02.ad.ydesigngroup.com/login";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -80,6 +82,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         return super.onOptionsItemSelected(item);
     }
+
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.sign_in_btn:
@@ -87,14 +90,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 TextView login = (TextView)findViewById(R.id.login_tf);
                 TextView pass = (TextView)findViewById(R.id.pass_tf);
                 String username = String.valueOf(login.getText());
-                String password = "";
-                try {
-                    MessageDigest digest = MessageDigest.getInstance("SHA-256");
-                    byte[] hash = digest.digest(String.valueOf(pass.getText()).getBytes(StandardCharsets.UTF_8));
-                    password = hash.toString();
-                }catch (NoSuchAlgorithmException nse){
-                    Toast.makeText(this,"Algorithm not supported",Toast.LENGTH_LONG).show();
-                }
+                String password = sha256(String.valueOf(pass.getText()));
                 if(password.length() > 0){
                     new postLoginClass().execute(username, password);
                 }
@@ -102,6 +98,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    /**
+     * Generates a SHA-256 Hash for any string
+     * @param base
+     * @return
+     */
+    private String sha256(String base) {
+        try{
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(base.getBytes("UTF-8"));
+            StringBuffer hexString = new StringBuffer();
+
+            for (int i = 0; i < hash.length; i++) {
+                String hex = Integer.toHexString(0xff & hash[i]);
+                if(hex.length() == 1) hexString.append('0');
+                hexString.append(hex);
+            }
+
+            return hexString.toString();
+        } catch (NoSuchAlgorithmException nse){
+             Toast.makeText(MainActivity.this,"Algorithm not supported",Toast.LENGTH_LONG).show();
+        }catch(Exception ex){
+            throw new RuntimeException(ex);
+        }
+        return null;
+    }
 
     public class postLoginClass extends AsyncTask<String, Integer, String> {
         String response = "NOK";
@@ -112,7 +133,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         @Override
         protected String doInBackground(String... params) {
-
             String post_url = LOGIN_URL;
 
             try {
@@ -121,13 +141,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 				
 				//set connection Header
                 conn.setRequestMethod("POST");
-				con.setRequestProperty("Accept", "application/json");
-				con.setRequestProperty("Content-type", "application/json")
+				//conn.setRequestProperty("Accept", "application/json");
+				conn.setRequestProperty("Content-Type", "application/json");
 				
 			    JSONObject jsonObject = new JSONObject();
 				jsonObject.put("username", params[0]);
 				jsonObject.put("password", params[1]);
-				
+				//System.out.println("username: "+params[0]+" password:"+params[1]);
 				//send post request
                 conn.setDoOutput(true);
                 DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
@@ -137,6 +157,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 	
                 String reply="FALSE";
                 int responseCode = conn.getResponseCode();
+                //System.out.println("Response CODE : "+responseCode);
                 if (responseCode == HttpURLConnection.HTTP_OK) { //success,200 code
 					BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
 					String inputLine;
@@ -146,13 +167,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 						response.append(inputLine);
 					}
 					in.close();
+                    //System.out.println(response); //gets the response
                     reply = String.valueOf(response);
                 }
 				conn.disconnect();
-                if (reply.equals("OK")){
-                    response = reply;
-                    return response;
-                }
+                response=reply;
+                return null;
             } catch (MalformedURLException e) {
                 e.printStackTrace();
             } catch (IOException ioe){
@@ -166,8 +186,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         @Override
         protected void onPostExecute(String result) {
-            // TODO Auto-generated method stub
-            Toast.makeText(MainActivity.this,"PostExecute"+":"+response,Toast.LENGTH_LONG).show();
+            //if response is not ok
+            if(!response.equals("OK")){
+                Toast.makeText(MainActivity.this,"Login Unsuccessful!! \nPlease Check Credentials!!",Toast.LENGTH_LONG).show();
+            }
+            else if (response.equals("OK")){
+                Toast.makeText(MainActivity.this, "PostExecute" + ":" + response, Toast.LENGTH_LONG).show();
+            }
             return;
         }
     }
